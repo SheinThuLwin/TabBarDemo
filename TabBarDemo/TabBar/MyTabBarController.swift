6be743c908e602ee7c9b92cb583aa561dca43f22
Shein <sheinthulwin@gmail.com>
2022-02-21 17:51:06 +0630

    Add style and selected-underline at tabBar.

--- TabBarDemo/TabBar/MyTabBarController.swift
+++ TabBarDemo/TabBar/MyTabBarController.swift
@@ -13,9 +13,43 @@ class MyTabBarController: UITabBarController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        self.tabBar.unselectedItemTintColor = .white
-        self.tabBar.tintColor = .orange
+        self.viewControllers = getTabViewControllers()
         
+        //It will show when background color is clean.
+        tabBar.layer.shadowColor = UIColor.red.cgColor
+        tabBar.layer.shadowOpacity = 0.8
+        tabBar.layer.shadowOffset = CGSize(width: 2, height: 2)//CGSize.zero
+        tabBar.layer.shadowRadius = 5
+        
+        self.tabBar.layer.borderColor = UIColor.yellow.cgColor
+        self.tabBar.layer.borderWidth = 2
+        
+        self.tabBar.clipsToBounds = false
+        self.tabBar.backgroundColor = .white
+        
+        UITabBar.appearance().shadowImage = UIImage()
+        UITabBar.appearance().backgroundImage = UIImage()
+        
+        self.tabBar.unselectedItemTintColor = .purple
+        self.tabBar.tintColor = .orange
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        let tabCount = CGFloat(tabBar.items?.count ?? 0)
+        if tabCount > 0{
+            let bottomSafeArea: CGFloat = self.view.safeAreaInsets.bottom
+            let tabWidth = tabBar.frame.width/tabCount
+            let tabHeight = tabBar.frame.height
+            let selectionIndicatorImage = UIImage().createSelectionIndicator(color: .orange,
+                                                                             size: CGSize(width: tabWidth, height: tabHeight),
+                                                                             lineHeight: 3.0,
+                                                                             bottomSafeArea: bottomSafeArea)
+
+            self.tabBar.selectionIndicatorImage = selectionIndicatorImage
+        }
+    }
+    
+    private func getTabViewControllers() -> [UIViewController]{
         let homeVC = HomeViewController(nibName: "HomeViewController", bundle: nil)
         let messageVC = MessageViewController(nibName: "MessageViewController", bundle: nil)
         
@@ -25,9 +59,8 @@ class MyTabBarController: UITabBarController {
         messageVC.tabBarItem.title = "Message"
         messageVC.tabBarItem.image = UIImage(named: "message")
         
-        self.viewControllers = [homeVC, messageVC]
+        return [homeVC, messageVC]
     }
-    
 
     /*
     // MARK: - Navigation
@@ -39,4 +72,16 @@ class MyTabBarController: UITabBarController {
     }
     */
 
+}
+
+extension UIImage {
+    //Ref: https://nitishrajput912.medium.com/underline-tabbar-ios-swift-1ae53ac4a75f
+    func createSelectionIndicator(color: UIColor, size: CGSize, lineHeight: CGFloat, bottomSafeArea: CGFloat) -> UIImage {
+        UIGraphicsBeginImageContextWithOptions(size, false, 0)
+        color.setFill()
+        UIRectFill(CGRect(x: 0, y: size.height - bottomSafeArea, width: size.width, height: lineHeight))
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return image!
+    }
 }
